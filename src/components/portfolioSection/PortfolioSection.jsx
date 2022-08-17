471a49ac436d25c1cba2bf3cb8c147a559379df4
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 00:28:16 +0300

    added porfolio section component and some styles to it

---
+++ src/components/portfolioSection/PortfolioSection.jsx
@@ -0,0 +1,27 @@
+import React, {useState, useEffect}from 'react';
+import SectionHeader from '../sectionHeader/SectionHeader';
+import axios from 'axios';
+import ProjectsContainer from './ProjectsContainer';
+import './PortfolioSection.css';
+const PortfolioSection = (props) => {
+  
+  return (
+    <section className='portfolio-section'>
+      <div className='container'>
+        <hr className='portfolio-section__line_dashed-top dashed-line' />
+
+        <SectionHeader
+          header="Portfolio"
+          subheader="Simplicity is the ultimate sophistication"
+          theme={props.theme}
+        />
+        <ProjectsContainer/>
+        
+        <hr className='portfolio-section__line_dashed-bottom dashed-line' />
+
+      </div>
+    </section>
+  );
+};
+
+export default PortfolioSection;
