bfcef8078d0591975c693302016398d63c59b78c
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 00:29:51 +0300

    added section with filters checkboxes for projects

---
+++ src/components/portfolioSection/FiltersSection.jsx
@@ -0,0 +1,16 @@
+import React, {useState}from 'react';
+
+const FiltersSection = (props) => {
+  return (
+    <div className='filters-section'>
+      <span className='filters-section__text'>Filter by</span>
+      <ul className='filters-section__technologies-list'>
+      {
+        props.children
+      }
+      </ul>
+    </div>
+  );
+};
+
+export default FiltersSection;
