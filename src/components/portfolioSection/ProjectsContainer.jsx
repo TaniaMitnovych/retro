7648e17df35bd9678e281fc0adf1ea5fb588a29e
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 00:26:53 +0300

    added container for project cards and styles

---
+++ src/components/portfolioSection/ProjectsContainer.jsx
@@ -0,0 +1,150 @@
+import React, { useState, useEffect, useRef } from 'react';
+import axios from 'axios';
+import ProjectCard from './ProjectCard';
+import projects from '../../projects.json';
+import './ProjectsContainer.css';
+import _ from 'lodash';
+import FiltersSection from './FiltersSection';
+import compareDesc from 'date-fns/compareDesc';
+const ProjectsContainer = () => {
+  const defaultAmountOfVisibleProjects = 6;
+  const [technologies, setTechnologies] = useState([]);
+  const [amountOfVisibleProjects, setAmountOfVisibleProjects] = useState(defaultAmountOfVisibleProjects);
+  const [filters, setFilters] = useState([]);
+  const [filteredPtoj, setFilteredProj] = useState([
+    ... (projects.filter(proj => {
+      let intersection = _.intersection(filters, proj.technologies);
+      return _.isEqual(filters, intersection);
+    }))
+  ]);
+  const [shownAllProj, setShown] = useState(false);
+  const [buttonText, setButtonText] = useState('Browse all');
+  useEffect(() => {
+    projects.map((proj) => {
+      setTechnologies(prev => [...(new Set([...prev, ..._.difference(proj.technologies, technologies)]))]);
+    })
+  }, [])
+  useEffect(() => {
+    setFilteredProj(filterProjects());
+    showProjects();
+  }, [filters])
+  useEffect(() => {
+    if (shownAllProj) {
+      setFilters([]);
+      //setAmountOfVisibleProjects(filteredPtoj.length);
+      setCheckedState(new Array(technologies.length).fill(true));
+      setButtonText("Hide projects");
+    } else {
+      //setAmountOfVisibleProjects(defaultAmountOfVisibleProjects);
+      setCheckedState(new Array(technologies.length).fill(false));
+      setButtonText("Browse all");
+    }
+  }, [shownAllProj])
+
+  const filterProjects = () => {
+    console.log(filters.length);
+    if (filters.length === 0) {
+      console.log(projects);
+      projects.sort((project1, project2) => {
+        let dat1 = new Date(project1.date),
+          dat2 = new Date(project2.date);
+        if (dat1 > dat2)
+          return -1;
+        else if (dat1 < dat2)
+          return 1;
+        else return 0;
+      })
+
+      return [...projects]
+    } else {
+      return [...(projects.filter(proj => {
+        let intersection = _.intersection(filters, proj.technologies);
+        console.log(intersection);
+        return _.isEqual(filters, intersection);
+      }))]
+    }
+  }
+  const handleFilterClick = (event) => {
+    if (filters.includes(event.target.value)) {
+      setFilters(prev => {
+        _.remove(prev, (filter) => { return filter === event.target.value });
+        return [...prev];
+      });
+      console.log(filters);
+      event.target.checked = false;
+    } else {
+      setFilters(prev => [...prev, event.target.value]);
+      console.log(filters);
+      event.target.checked = true;
+    }
+    setFilteredProj(filterProjects());
+  }
+
+  const showProjects = () => {
+    let amountOfProjectsToShow = amountOfVisibleProjects;
+    if (filteredPtoj.length < amountOfVisibleProjects &&
+      filteredPtoj.length > 0) {
+      amountOfProjectsToShow = filteredPtoj.length;
+    }
+    else if(filteredPtoj.length > amountOfVisibleProjects &&
+      shownAllProj){
+      amountOfProjectsToShow=filteredPtoj.length;
+      //setAmountOfVisibleProjects(defaultAmountOfVisibleProjects);
+    }
+    console.log(filteredPtoj.length);
+    console.log(amountOfProjectsToShow);
+    if (filteredPtoj.length === 0) {
+      return <p>Nothing was found</p>
+    } else {
+      return _.times(amountOfProjectsToShow, (i) => {
+        return (
+          <ProjectCard key={filteredPtoj[i].name}
+            image={filteredPtoj[i].image_url}
+            name={filteredPtoj[i].name}
+            date={filteredPtoj[i].date}
+            technologies={filteredPtoj[i].technologies.join(', ')}
+            cost={filteredPtoj[i].cost}
+          />)
+      })
+    }
+  }
+  const [checkedState, setCheckedState] = useState(
+    new Array(technologies.length).fill(false)
+  );
+  const handleOnChange = (position) => {
+    const updatedCheckedState = checkedState.map((item, index) =>
+      index === position ? !item : item
+    );
+    setCheckedState(updatedCheckedState);
+  }
+  return (
+    <div className='projects-container'>
+      <FiltersSection>
+        {
+          technologies.map((technology, i) => {
+            return (
+              <li className='filters-section__technology-item' key={technology}>
+                <input
+                  type='checkbox'
+                  id={technology}
+                  name="technology"
+                  value={technology}
+                  onChange={(event) => {handleFilterClick(event); handleOnChange(i)}}
+                  checked={checkedState[i]}
+                />
+                <label htmlFor={technology}>{technology}</label>
+              </li>
+            )
+          })
+        }
+      </FiltersSection>
+      {
+        showProjects()
+      }
+      <hr />
+      <button type='button' onClick={() => { setShown(prev => { return prev ? false : true }) }}>{buttonText}</button>
+    </div>
+  );
+};
+
+export default ProjectsContainer;
