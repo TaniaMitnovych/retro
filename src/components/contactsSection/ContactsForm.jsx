66dbc32743c3424b3014b216fda3fbc06462d991
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 20:53:52 +0300

    added contacts section

--- src/components/contactsSection/ContactsForm.jsx
+++ src/components/contactsSection/ContactsForm.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import './ContactsForm.css';
 const ContactsForm = (props) => {
   return (
     <form action='' method='post' id='contacts-form' className='contacts-form'>
-      <input className='contacts-form__input' type='text' value='name' placeholder="Name" name='name' id='name'/>
-      <input className='contacts-form__input' type='email' value='email' placeholder='Email address' name='email' id='email'/>
-      <input className='contacts-form__input' type='text' value='subject' placeholder='Subject:' name='subject' id='subject'/>
-      <textarea className='contacts-form__text' form='contacts-form' value='message' rows='7' name='message' id='message'>
+      <input className='contacts-form__input' type='text'  placeholder="Name" name='name' id='name'/>
+      <input className='contacts-form__input' type='email'  placeholder='Email address' name='email' id='email'/>
+      <input className='contacts-form__input' type='text'  placeholder='Subject:' name='subject' id='subject'/>
+      <textarea className='contacts-form__text' form='contacts-form'  rows='6' name='message' id='message'>
 
       </textarea>
       <button type='submit' className='contacts-form__submit-button'>Send Message</button>
--- src/components/contactsSection/ContactsSection.jsx
+++ src/components/contactsSection/ContactsSection.jsx
@@ -6,26 +6,31 @@ import facebookIcon from '../../images/facebook.png';
 import gplusIcon from '../../images/gplus.png';
 import linkedInIcon from '../../images/linkedin.png';
 import pinterest from '../../images/pinterest.png';
-
+import './ContactsSection.css';
 const ContactsSection = (props) => {
   return (
-    <section className='contacts-section' >
+    <section className='contacts-section' id='contacts' >
       <div className='container'>
+        <hr className='contacts-section__line_dashed-top dashed-line' />
+
         <SectionHeader
-          theme={props.theme}
+          color='light'
+          theme='dark'
           header="Contact me"
           subheader="I'll be glad to answer your questions!"
         />
         <ContactsForm />
+
+        <p className='contacts-section__text'>I am social</p>
+        <ul className='contacts-section__social-list'>
+          <li className='contacts-section__social-item'><a href="https://twitter.com/?lang=uk"><img src={twitterIcon} /></a></li>
+          <li className='contacts-section__social-item'><a href="https://m.facebook.com/profile.php"><img src={facebookIcon} /></a></li>
+          <li className='contacts-section__social-item'><a href="mailto:taniamitnovych15@gmail.com"><img src={gplusIcon} /></a></li>
+          <li className='contacts-section__social-item'><a href="https://www.pinterest.ca/"><img src={pinterest} /></a></li>
+          <li className='contacts-section__social-item'><a href="https://www.linkedin.com/in/tetiana-mitnovych-743850235/"><img src={linkedInIcon} /></a></li>
+        </ul>
+        <hr className='contacts-section__line_dashed-bottom dashed-line' />
       </div>
-      <p>I am social</p>
-      <ul className='contacts-section__social-list'>
-        <li className='contacts-section__social-item'><a href="https://twitter.com/?lang=uk"><img src={twitterIcon}/></a></li>
-        <li className='contacts-section__social-item'><a href="https://m.facebook.com/profile.php"><img src={facebookIcon}/></a></li>
-        <li className='contacts-section__social-item'><a href="mailto:taniamitnovych15@gmail.com"><img src={gplusIcon}/></a></li>
-        <li className='contacts-section__social-item'><a href="https://www.linkedin.com/in/tetiana-mitnovych-743850235/"><img src={linkedInIcon}/></a></li>
-        <li className='contacts-section__social-item'><a href="https://www.pinterest.ca/"><img src={pinterest}/></a></li>
-      </ul>
     </section>
   );
 };
