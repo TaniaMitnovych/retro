8784ecfcfe3bc73e8a8b9cd71cc84d34fd96ebe4
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 20:53:00 +0300

    fixed some bugs with infinite scroll

--- src/components/blogSection/BlogSection.jsx
+++ src/components/blogSection/BlogSection.jsx
@@ -4,6 +4,7 @@ import posts from '../../posts.json';
 import { useState, useEffect, useRef, useMemo } from 'react';
 import _ from 'lodash';
 import PostCard from './PostCard';
+import './BlogSection.css';
 
 const BlogSection = (props) => {
   const buttonTarget = useRef(null);
@@ -11,31 +12,29 @@ const BlogSection = (props) => {
   let amountOfPosts = 20;
   const [currentPage, setPage] = useState(0);
   const pageSize = 3;
-  const [postsList, setPostsList] = useState([
-    //...(_.slice(posts, currentPage * pageSize, currentPage * pageSize + pageSize))
-  ]);
-  const [shownAllPosts, setShownAll]=useState(false);
-  const [buttonText, setButtonText]=useState('Show more posts');
+  const [postsList, setPostsList] = useState([]);
+  const [shownAllPosts, setShownAll] = useState(false);
+  const [buttonText, setButtonText] = useState('Show more posts');
 
   const loadPosts = () => {
     setPage(prev => { return prev + 1 });
     console.log(currentPage);
     setShownAll(true);
     console.log(postsList);
-    
+
   }
-  const handleButtonClick=()=>{
-    if(shownAllPosts===true){
+  const handleButtonClick = () => {
+    if (shownAllPosts === true) {
       setPostsList([]);
       setPage(0);
       console.log(currentPage);
     }
   }
-  useEffect(()=>{
+  useEffect(() => {
     setButtonText('Hide posts');
   }, [shownAllPosts]);
 
-  useEffect(()=>{
+  useEffect(() => {
     setPostsList(prev => [...prev, ...(_.slice(posts, currentPage * pageSize, currentPage * pageSize + pageSize))]);
   }, [currentPage]);
 
@@ -45,30 +44,35 @@ const BlogSection = (props) => {
 
 
   return (
-    <div className={`blog-section blog-section_${props.theme}`}>
-      <SectionHeader
-        theme={props.theme}
-        header="My blog"
-        subheader="News from my house"
-      />
-      <div className='blog-section__container'>
-        {
-          postsList.map((post) => {
-            return (
-              <PostCard
-                theme={props.theme}
-                image={post.image}
-                title={post.title}
-                responses={post.responses}
-                info={post.date + ' // ' + post.role + ' // ' + post.tags.join(', ')}
-                text={post.text}
-              />
-            )
-          })
-        }
+    <section className={`blog-section blog-section-${props.theme}`} id='blog'>
+      <div className='container'>
+        <hr className='blog-section__line_dashed-top dashed-line' />
+        <SectionHeader
+          color='dark'
+          theme={props.theme}
+          header="My blog"
+          subheader="News from my house"
+        />
+        <div className='blog-section__container'>
+          {
+            postsList.map((post) => {
+              return (
+                <PostCard
+                  theme={props.theme}
+                  image={post.image}
+                  title={post.title}
+                  responses={post.responses}
+                  info={post.date + ' // ' + post.role + ' // ' + post.tags.join(', ')}
+                  text={post.text}
+                />
+              )
+            })
+          }
+        </div>
+        <button className='blog-section__button' ref={buttonTarget} type='button' onClick={handleButtonClick}>{buttonText}</button>
+        <hr className='blog-section__line_dashed-bottom dashed-line' />
       </div>
-      <button ref={buttonTarget} type='button' onClick={handleButtonClick}>{buttonText}</button>
-    </div>
+    </section>
   );
 };
 
--- src/components/blogSection/PostCard.jsx
+++ src/components/blogSection/PostCard.jsx
@@ -1,22 +1,66 @@
-import React from 'react';
-
+import React, { useState, useEffect, useMemo, useRef } from 'react';
+import './PostCard.css';
 const PostCard = (props) => {
+  const postRef = useRef(null);
+  const isInViewport = useIsInViewport(postRef);
+  useEffect(() => {
+    if(isInViewport){
+      showPost();
+    }else{
+      postRef.current.style.opacity="0";
+    }
+  }, [isInViewport]);
+
+  const showPost=()=>{
+    let opacity=0;
+    let timer=setInterval(()=>{
+      if(opacity===1){
+        clearInterval(timer);
+      }else{
+        postRef.current.style.opacity=(opacity+=0.01);
+      }
+    },10)
+    
+  }
   return (
-    <div className={`post-card post-card_${props.theme}`}>
+    <div ref={postRef} className={`post-card post-card_${props.theme}`}>
       <img src={props.image} alt='' className='post-card__image' />
       <div className='post-card__content'>
         <h4 className='post-card__title'>{props.title}</h4>
+
+        <p className='post-card__info'>{props.info}</p>
         <p className='post-card__responses'>
           <a href='#'>
-            {props.responses === 0 ? 'no responses' : (props.responses === 1 ? '1 response' : (props.responses + 'responses'))}
+            {props.responses === 0 ? 'no responses' : (props.responses === 1 ? '1 response' : (props.responses + ' responses'))}
           </a>
         </p>
-        <p className='post-card__info'>{props.info}</p>
+        <hr />
         <p className='post-card__text'>{props.text}</p>
         <span className='post-card__show-more'><a href='#'>(More...)</a></span>
       </div>
     </div>
   );
 };
+function useIsInViewport(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  const observer = useMemo(
+    () =>
+      new IntersectionObserver(([entry]) =>
+        setIsIntersecting(entry.isIntersecting),
+      ),
+    [],
+  );
+
+  useEffect(() => {
+    observer.observe(ref.current);
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [ref, observer]);
+
+  return isIntersecting;
+}
 
 export default PostCard;
