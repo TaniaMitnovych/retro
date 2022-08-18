4da1a91fa0f87df8bda88d9ff2479b00bfeef2a9
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 20:58:24 +0300

    modified some files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, Suspense, useRef } from 'react';
+import './reset.css';
 import './App.css';
+import Header from './components/header/Header';
+import Loader from './components/loader/Loader';
+import ReadingBar from './components/readingBar/ReadingBar';
+import Ribbon from './components/ribbon/Ribbon';
+import SliderSection from './components/sliderSection/SliderSection';
+import AboutIcon from './components/icons/AboutIcon';
+import tvImage from './images/tv.png';
+import AboutSection from './components/aboutSection/AboutSection';
+import PortfolioSection from './components/portfolioSection/PortfolioSection';
+import BlogSection from './components/blogSection/BlogSection';
+import ContactsSection from './components/contactsSection/ContactsSection';
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState();
+  let currentHour = (new Date(Date.now())).getHours();
+  const [theme, setTheme] = useState(
+    currentHour >= 21 && currentHour <= 6
+      ? 'dark'
+      : 'light'
+  );
+  const switchTheme=()=>{
+    setTheme(prev=>{return prev==='dark'?'light':'dark'});
+  }
+  const getPage = () => {
+    return (
+      <>
+        <button className='theme-switcher' type='button' onClick={switchTheme}>{theme==='dark'?'Light':"Dark"}</button>
+        <Header theme={theme} />
+        <SliderSection theme={theme}/>
+        <AboutSection theme={theme} />
+        <PortfolioSection theme={theme} />
+        <BlogSection theme={theme} />
+        <ContactsSection />
+      </>
+    )
+  }
+  useEffect(()=>{
+    setPage(getPage);
+  }, [theme])
+  useEffect(() => {
+    setTimeout(()=>{
+      setPage(getPage);
+    }, 5000)
+    
+  },[])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense>
+      <div className="App">
+        <Loader time='5000' />
+        {page}
+      </div>
+    </Suspense>
+
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,16 +4,16 @@ import './Header.css';
 import ReadingBar from '../readingBar/ReadingBar';
 const Header = (props) => {
   return (
-    <div className={`header header-dark`}>
+    <div className={`header header-${props.theme}`}>
       <div className='container header__container'>
         <ul className='header__list header__list-left'>
-          <li><a href=''>About me</a></li>
-          <li><a href=''>Portfolio</a></li>
+          <li><a href='#about'>About me</a></li>
+          <li><a href='#portfolio'>Portfolio</a></li>
         </ul>
         <Logo />
         <ul className='header__list header__list-right'>
-          <li><a href=''>My Blog</a></li>
-          <li><a href=''>Contact me</a></li>
+          <li><a href='#blog'>My Blog</a></li>
+          <li><a href='#contacts'>Contact me</a></li>
         </ul>
       </div>
       <hr className='header__line_dashed dashed-line'/>
