055d81206f047604a6b581dae5209589e38dbfde
TaniaMitnovych <taniamitnovych15@gmail.com>
2022-08-18 20:55:40 +0300

    changed styles to section header

--- src/components/sectionHeader/SectionHeader.jsx
+++ src/components/sectionHeader/SectionHeader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './SectionHeader.css';
-import separator from '../../images/Separator.png';
+import separator_dark from '../../images/Separator.png';
+import separator_light from '../../images/Separator_light.png';
 const SectionHeader = (props) => {
   return (
     <div className={`section-header section-header_${props.theme}`}>
       <h2 className='section-header__header'>{props.header}</h2>
-      <img src={separator} alt='' className='section-header__separator' />
+      <img src={props.color==='dark'?separator_dark:separator_light} alt='' className='section-header__separator' />
       <h3 className='section-header__subheader'>{props.subheader}</h3>
     </div>
   );
